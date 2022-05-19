16b1684e8482c2bbc324ec4ddd89cddbefb517ee
Miles Morlock <milesmorlockcode@gmail.com>
2022-05-19 15:02:34 -0400

    On the client side, database data can be accessed through ough the /getTodos path

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ const collection = "todo";
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res) => {
+    db.getDb().collection(collection).find({}).toArray((err, documents) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).json(documents);
+        }
+    });
+});
+
 db.connect(err => {
     if (err) {
         console.log('Something went wrong ' + err);
@@ -19,4 +33,4 @@ db.connect(err => {
             console.log('App listening on PORT 3000...');
         })
     }
-})
+});
